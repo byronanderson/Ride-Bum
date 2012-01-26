55e4b55951527264c02847bbc5c3c55fd95ebdba
Byron Anderson <byronanderson32@gmail.com>
2012-01-25 16:14:29 -0800

    Initial, empty import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RideBum::Application.config.secret_token = '70d3c516777c224b2b6d09c3f6028dcd9dc3f13994501942ecbd1c649ee5c0ae4ec443720e56113aa8939b1af55f7b867905e4d61303c1e8345078958baf8cce'
