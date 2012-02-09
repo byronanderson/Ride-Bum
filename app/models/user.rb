8533cc5087091bb9024f21cc4624c498687b119a
Han <hkfortytwo@gmail.com>
2012-02-09 14:17:20 -0800

    added user auth, bootstrap, and basic nav bar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :login, :username, :email, :password, :password_confirmation, :remember_me
+	attr_accessor :login
+	protected
+	def self.find_for_database_authentication(warden_conditions)
+		conditions = warden_conditions.dup
+		login = conditions.delete(:login)
+		where(conditions).where(['lower(username) = :value OR lower(email) = :value', { :value => login.downcase }]).first
+	end
 end
