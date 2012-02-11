129e2dae5ec56fca240bfb9d11b577e28ac31f22
Byron Anderson <byronanderson32@gmail.com>
2012-02-11 13:56:17 -0800

    partially in place, needs to see jquery javascript and I am looking for that google hosting url

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ActiveRecord::Base
   validates :name,          presence: true
   validates :destination,   presence: true
   validates :arrival_time,  presence: true
+
+  alias_attribute :time, :arrival_time
 end
