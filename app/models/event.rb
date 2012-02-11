fc1b9b0645cd99b5d7189d46292e4f8f0193b154
Byron Anderson <byronanderson32@gmail.com>
2012-02-11 13:25:00 -0800

    Basic shape of the solution in place

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ActiveRecord::Base
   validates :name,          presence: true
   validates :destination,   presence: true
   validates :arrival_time,  presence: true
+
+  alias_attribute :date, :arrival_time
+  alias_attribute :time, :arrival_time
 end
