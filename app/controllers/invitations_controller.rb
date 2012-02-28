1ed3a57b4756e84019f8a9d24b05194a8b385352
Byron Anderson <byronanderson32@gmail.com>
2012-02-27 20:56:55 -0800

    Adding invitations controller scaffold, implemented enough for invitation creation but yet enough for email sending

---
+++ app/controllers/invitations_controller.rb
@@ -0,0 +1,83 @@
+class InvitationsController < ApplicationController
+  # GET /invitations
+  # GET /invitations.json
+  def index
+    @invitations = Invitation.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @invitations }
+    end
+  end
+
+  # GET /invitations/1
+  # GET /invitations/1.json
+  def show
+    @invitation = Invitation.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @invitation }
+    end
+  end
+
+  # GET /invitations/new
+  # GET /invitations/new.json
+  def new
+    @invitation = Invitation.new(event: Event.find(params[:event_id]))
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @invitation }
+    end
+  end
+
+  # GET /invitations/1/edit
+  def edit
+    @invitation = Invitation.find(params[:id])
+  end
+
+  # POST /invitations
+  # POST /invitations.json
+  def create
+    @invitation = Invitation.new(params[:invitation])
+
+    respond_to do |format|
+      if @invitation.save
+        format.html { redirect_to new_event_invitation_path(@invitation.event, Invitation.new(event: @invitation.event)), notice: "#{@invitation.invitee_name} has been added" }
+        format.json { render json: @invitation, status: :created, location: @invitation }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @invitation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /invitations/1
+  # PUT /invitations/1.json
+  def update
+    @invitation = Invitation.find(params[:id])
+
+    respond_to do |format|
+      if @invitation.update_attributes(params[:invitation])
+        format.html { redirect_to @invitation, notice: 'Invitation was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @invitation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /invitations/1
+  # DELETE /invitations/1.json
+  def destroy
+    @invitation = Invitation.find(params[:id])
+    @invitation.destroy
+
+    respond_to do |format|
+      format.html { redirect_to invitations_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ app/models/invitation.rb
@@ -0,0 +1,3 @@
+class Invitation < ActiveRecord::Base
+  belongs_to :event
+end
