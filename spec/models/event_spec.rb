e3aa1235b60539380654227148587ec2414076f9
Cynthia Kiser <cnk@ugcs.caltech.edu>
2012-03-22 22:03:55 -0700

    Update to Rails 3.2.2; Update Factory to FactoryGirl to get rid of warnings

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe Event do
   context "Required fields" do
     it "should require name" do
-      Factory.build(:event, name: nil).should_not be_valid
+      FactoryGirl.build(:event, name: nil).should_not be_valid
     end
     it "should require destination" do
-      Factory.build(:event, destination: nil).should_not be_valid
+      FactoryGirl.build(:event, destination: nil).should_not be_valid
     end
     it "should require arrival time" do
-      Factory.build(:event, arrival_time: nil).should_not be_valid
+      FactoryGirl.build(:event, arrival_time: nil).should_not be_valid
     end
   end
 end
